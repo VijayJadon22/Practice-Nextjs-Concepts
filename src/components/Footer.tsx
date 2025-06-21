da4d126101167ead93f638b6542a75043e35d0da
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-06-21 15:41:47 +0530

    Created a component folder inside src and created Header and Footer components

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="flex justify-center items-center mt-auto border-black/10 border-t h-[40px] px-5">
+      Footer
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <div className="border-black/10 border-b h-[40px] flex items-center justify-between px-5">
+      <div>Logo</div>
+      <ul className="flex items-center gap-3">
+        <li>
+          <Link href={"/"}>Home</Link>
+        </li>
+        <li>
+          <Link href={"/posts"}>Posts</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Header;
